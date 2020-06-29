7bb684aed5f76b19a37cd0b3a77f824ff5655625
TouKhen <jack.lecomtes@gmail.com>
2020-06-29 17:01:52 +0200

    Carousel fixed

--- assets/js/scriptAppart1.js
+++ assets/js/scriptAppart1.js
@@ -1,35 +1,17 @@
 // Carousel
+let bigCard = document.getElementsByClassName("big-card");
 let bigImg = document.getElementById("bigImg");
 let carouselItem = document.getElementsByClassName("carousel-item");
 
-carouselItem[0].addEventListener('mousedown', carouselSelector1);
+bigCard[0].addEventListener('mousedown', carouselSelector);
 
-function carouselSelector1() {
-    bigImg.src = "../img/pic1.png";
-}
-
-carouselItem[1].addEventListener('mousedown', carouselSelector2);
-
-function carouselSelector2() {
-    bigImg.src = "../img/pic2.png";
-}
-
-carouselItem[2].addEventListener('mousedown', carouselSelector3);
-
-function carouselSelector3() {
-    bigImg.src = "../img/pic3.png";
-}
-
-carouselItem[3].addEventListener('mousedown', carouselSelector4);
-
-function carouselSelector4() {
-    bigImg.src = "../img/pic4.png";
-}
-
-carouselItem[4].addEventListener('mousedown', carouselSelector5);
-
-function carouselSelector5() {
-    bigImg.src = "../img/pic5.png";
+function carouselSelector() {
+    for (let i = 0; i < carouselItem.length; i++){
+        carouselItem[i].addEventListener('mouseup', carouselSelected);
+        function carouselSelected(){
+            bigImg.src = "../img/pic" + (i + 1) + ".png";
+        }
+    }
 }
 
 //Darken image
